a1a8d9efbb24750587f0263e87f99f49e732915a
SimplyEman <53872061+SimplyEman@users.noreply.github.com>
2025-06-25 21:10:22 +0100

    Add files via upload

--- utils.py
+++ utils.py
@@ -5,3 +5,10 @@ def get_change_codes():
     file_path = os.path.join(base_path, "data", "change_codes.txt")
     with open(file_path, "r") as f:
         return [line.strip() for line in f if line.strip()]
+
+def load_msg_as_text(msg_file):
+    import extract_msg
+    msg = extract_msg.Message(msg_file)
+    msg_message = msg.body
+    attachments = msg.attachments
+    return msg_message, attachments
