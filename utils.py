d03644f2d3e8d1132663ad2c7f7fa88713440012
SimplyEman <53872061+SimplyEman@users.noreply.github.com>
2025-06-25 22:53:05 +0100

    Add files via upload

--- utils.py
+++ utils.py
@@ -6,6 +6,14 @@ PRODUCT_MAP = {
     f"PL 1000/{1000+i}": f"Product {i+1}" for i in range(100)
 }
 
+# Static map of change codes to fee types
+FEE_TYPE_MAP = {
+    "Change Code 1": "Minor",
+    "Change Code 2": "Major",
+    "Change Code 3": "Grouping",
+    "Change Code 4": "Worksharing"
+}
+
 def get_change_codes():
     base_path = os.path.dirname(__file__)
     file_path = os.path.join(base_path, "data", "change_codes.txt")
@@ -20,3 +28,6 @@ def load_msg_as_text(msg_file):
 
 def get_product_name(pl_number):
     return PRODUCT_MAP.get(pl_number, "")
+
+def get_fee_type(change_code):
+    return FEE_TYPE_MAP.get(change_code, "Unknown")
